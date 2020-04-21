14dc98bf72da590a38ef66261048d18446e0e078
mb_cardoso <mb.cardoso@campus.fct.unl.pt>
2020-04-21 17:16:48 +0100

    Deploying to FireBase

---
+++ quizz/dist/precache-manifest.025dfa701027a8d4be0a1a29823b0ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2942d4d435701dbeb2c212184fa240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5981e835fcd63fc094be",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "be85144c6ef3ae37d77f",
+    "url": "/static/js/2.c937230b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c937230b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5981e835fcd63fc094be",
+    "url": "/static/js/main.b4a2b98a.chunk.js"
+  },
+  {
+    "revision": "948649e694afa2df89d2",
+    "url": "/static/js/runtime-main.1deaf6bc.js"
+  }
+]);
